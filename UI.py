c178d4ee601eebe7764a57fa4599470f19107b9e
DeonXavierPereira <deonxavier2019@gmail.com>
2022-12-22 17:34:54 +0530

    Minor update

--- UI.py
+++ UI.py
@@ -1,17 +1,19 @@
 from tkinter import*
-global start_frame
+#import main.py
 root=Tk()
 root.title("QUIZOOO")
-def home():
-    start_frame=Frame(root,height=scrh,width=scrw)
+def home():#defining The home page
+    start_frame=Frame(root,height=scrh,width=scrw,bg="#ebac4d")
     start_frame.pack()
+    exitbut=Button(start_frame,text="Exit",font=("Times New Roman",25,"bold"),activebackground="#247371",bg="#4debe8",command=lambda:root.destroy())
+    exitbut.place(relx=0.5,rely=0.9,anchor=S)
+    
+    
 
 scrh=root.winfo_screenheight()  # Getting screen height
 scrw=root.winfo_screenwidth()   # Getting Screen width
 
-root.geometry("%dx%d"%(scrw,scrh))
-temp=start_frame=Frame(root,height=scrh,width=scrw,bg="#ebac4d")
-temp.pack()
-#root.attributes('-fullscreen',True)
+root.geometry("%dx%d"%(scrw,scrh))#Setting window to full screen
 home()
+print("Start Screen Loaded")
 root.mainloop()
