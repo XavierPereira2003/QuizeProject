030f93e30c5ebfbf0f3726eedbd23ffc149a2951
DeonXavierPereira <66835739+XavierPereira2003@users.noreply.github.com>
2022-12-22 15:41:34 +0530

    Test

---
+++ UI.py
@@ -0,0 +1,13 @@
+from tkinter import*
+root=Tk()
+root.title("QUIZOOO")
+def home():
+    start_frame=Frame(root)
+    start_frame.pack()
+    
+scrh=root.winfo_screenheight()  # Getting screen height
+scrw=root.winfo_screenwidth()   # Getting Screen width
+
+
+
+root.mainloop()
