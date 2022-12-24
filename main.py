d2b250b5c358fe2921ee88be55b65a31c87652b1
DeonXavierPereira <deonxavier2019@gmail.com>
2022-12-24 08:52:48 +0530

    Uploading files

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-from tkinter import *
+"""
+JSON file format 
+[
+    {
+    "ques":"Question",
+    "ans":"ans",
+    "dis":[,,,,,]
+    
+    },
+    
 
-class quesbut():
-    def __ini__():
+]
+
+"""
+from tkinter import *
+#Preprocessing
+root=Tk()
+scrh=root.winfo_screenheight()  # Getting screen height
+scrw=root.winfo_screenwidth()   # Getting Screen width
+
+root.geometry("%dx%d"%(scrw,scrh))
+
+# class quesbut():
+#     def __ini__():
         
+def main():
+    question_fram=Frame(root,height=scrh,width=scrw*0.66666,bg="#ebac4d")
+    question_fram.pack()
+main()
+# #with open("question.json") as question:
+    
+root.mainloop()
