7979b7371f835357947ae1dc7e7e7fb86efa5ef5
DeonXavierPereira <deonxavier2019@gmail.com>
2023-01-18 13:55:14 +0530

    minor addition on adding question

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-"""
-JSON file format 
-[
-    {
-    "ques":"Question",
-    "ans":"ans",
-    "dis":[,,,,,]
-    
-    },
-    
-
-]
 
 """
-# class quesbut():
-#     def __ini__():
+This is the functional unit of the project where all the logic for cheaking for correct answer
+
+
+"""
+class Question:
+    """
+    This class is intended for making, displaying and changing the questions seen on the quize page.
+    """
+    def __init__(self,frame,quest,ans,dis):
+        self.QuestionLabel=Label(frame,text="Hello",width=300,height=2,font=("garamond",80,'italic'),bg='#dec06f')
+
+    def pack(self):
+        self.pack()
+    def destroy(self):
+        self.destroy()
+
+
 
 def main():
-    def timer():
+    """
+    This is functional unit of the program. In here we build the main quiz page.
+    """
+    def timer():#This is the functional timer for the project.
         global Time
         Time=30
         while Time!=0:
@@ -35,17 +41,31 @@ def main():
     t1=threading.Thread(target=timer)
     t1.start()
 
-# #with open("question.json") as question:
+# #
 from tkinter import *
 from time import sleep
 import threading
-
+import json
+from random import randint
 #Preprocessing
 root=Tk()
 scrh=root.winfo_screenheight()  # Getting screen height
 scrw=root.winfo_screenwidth()   # Getting Screen width
 root.geometry("%dx%d"%(scrw,scrh))
 
+"""
+In this part we are choosing the questions
+"""
+Question=[]
+with open("question.json") as quest:
+    quest=json.load(quest)
+    for i in range(10):
+        #This part is done so no same question appear twice
+        while(True):
+            k=randint(0,len(quest)-1)
+            if quest(k) not in Question:
+                Question.append(quest[i])
+                break
 
 main()
  
